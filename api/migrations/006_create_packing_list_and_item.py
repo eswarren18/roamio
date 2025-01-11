149c45fe2ab81381fd42cfd48ce534562efadda7
Eric Warren <eswarren18@gmail.com>
2025-01-11 11:50:23 -0700

    added migration tables for packing_list and packing_item

---
+++ api/migrations/006_create_packing_list_and_item.py
@@ -0,0 +1,32 @@
+steps = [
+    [
+        """
+        CREATE TABLE packing_list (
+            id SERIAL PRIMARY KEY NOT NULL,
+            title VARCHAR(100) NOT NULL,
+            template BOOLEAN DEFAULT true
+        );
+        """,
+        """
+        DROP TABLE packing_list;
+        """
+    ],
+    [
+        """
+        CREATE TABLE packing_item (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            category VARCHAR(100),
+            location VARCHAR(100),
+            packed BOOLEAN DEFAULT false,
+            purchased BOOLEAN DEFAULT true,
+            notes VARCHAR(500),
+            packing_list_id INTEGER NOT NULL,
+            CONSTRAINT fk_packing_list FOREIGN KEY (packing_list_id) REFERENCES packing_list (id)
+        );
+        """,
+        """
+        DROP TABLE packing_item;
+        """
+    ]
+]
