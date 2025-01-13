3bf2471bed2ad12d29d5c86dffa7943d8b53d10b
Gregory <gregreinis@yahoo.com.here>
2025-01-13 15:10:47 -0800

    NOT WORKING - Flights backend API endpoints

--- api/queries/flights_queries.py
+++ api/queries/flights_queries.py
@@ -6,7 +6,7 @@ from psycopg.rows import class_row
 from fastapi import HTTPException
 
 class FlightsQueries:
-    def create(self, flight: FlightIn) -> FlightOut:
+    def create(self, flight: FlightIn, user_id: int) -> FlightOut:
         try:
             with pool.connection() as conn:
                 with conn.cursor(row_factory=class_row(FlightOut)) as cur:
@@ -16,17 +16,111 @@ class FlightsQueries:
                             (flight_number, departure_time, arrival_time, trip_id)
                         VALUES
                             (%s, %s, %s, %s)
+                        SELECT t.id
+                        FROM trips t
+                        WHERE f.trip_id = t.id
                         RETURNING *;
                         """,
                         [
                             flight.flight_number,
                             flight.departure_time,
                             flight.arrival_time,
-                            flight.trip_id
+                            flight.trip_id,
+                            user_id
                         ]
                     )
                     new_flight = cur.fetchone()
                     return new_flight
+        except Exception as e:
+            print(f"Error: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    def update(self, flight_id: int, flight: FlightIn) -> FlightOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=class_row(FlightOut)) as cur:
+                    cur.execute(
+                        """
+                        UPDATE flights
+                        SET id = %s, flight_number = %s, departure_time = %s, arrival_time = %s, trip_id = %s
+                        WHERE id = %s
+                        RETURNING *;
+                        """,
+                        [
+                            flight_id,
+                            flight.flight_number,
+                            flight.departure_time,
+                            flight.arrival_time,
+                            flight.trip_id,
+                            flight_id
+                        ]
+                    )
+                    updated_flight = cur.fetchone()
+                    return updated_flight
+        except Exception as e:
+            print(f"Error: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    def get_all(self, user_id: int) -> List[FlightOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=class_row(FlightOut)) as cur:
+                    cur.execute(
+                        """
+                        SELECT f.*
+                        FROM flights f
+                        JOIN trips t ON f.trip_id = t.id
+                        WHERE t.user_id = %s;
+                        """,
+                        [user_id]
+                    )
+                    flights = cur.fetchall()
+                    return flights
+        except Exception as e:
+            print(f"Error: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    def get_one(self, flight_id: int, user_id: int) -> FlightOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=class_row(FlightOut)) as cur:
+                    cur.execute(
+                        """
+                        SELECT f.id, f.departure_time, f.arrival_time,
+                           f.trip_id, f.flight_number
+                        FROM flights f
+                        JOIN trips t ON f.trip_id = t.id
+                        WHERE f.id = %s AND t.user_id = %s;
+                        """,
+                        [flight_id, user_id]
+                    )
+                    flight = cur.fetchone()
+                    if flight is None:
+                        raise HTTPException(status_code=404, detail="Flight not found")
+                    return flight
+        except HTTPException as http_exc:
+            raise http_exc
+        except Exception as e:
+            print(f"Error: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    def delete(self, flight_id: int, user_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        DELETE FROM flights f
+                        USING trips t
+                        WHERE f.trip_id = t.id
+                        """,
+                       # [flight_id, user_id]
+                    )
+                    if cur.rowcount == 0:
+                        raise HTTPException(status_code=404, detail="Flight not found")
+                    return True
+        except HTTPException as http_exc:
+            raise http_exc
         except Exception as e:
             print(f"Error: {e}")
             raise HTTPException(status_code=500, detail="Internal Server Error")
--- api/routers/flights_router.py
+++ api/routers/flights_router.py
@@ -22,5 +22,52 @@ async def create_flight(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Not logged in"
         )
-    new_flight = queries.create(flight)
+    new_flight = queries.create(flight, user.id)
     return new_flight
+
+@router.put("/{flight_id}", response_model=FlightOut)
+async def update_flight(
+    flight_id: int,
+    flight: FlightIn,
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: FlightsQueries = Depends()
+) -> FlightOut:
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not logged in"
+        )
+    updated_flight = queries.update(flight_id, flight)
+    return updated_flight
+
+@router.get("", response_model=List[FlightOut])
+async def get_flights(
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: FlightsQueries = Depends()
+) -> List[FlightOut]:
+    flights = queries.get_all(user.id)
+    return flights
+
+@router.get("/{flight_id}", response_model=FlightOut)
+async def get_flight(
+    flight_id: int,
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: FlightsQueries = Depends()
+) -> FlightOut:
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not logged in"
+        )
+    flight = queries.get_one(flight_id, user.id)
+    return flight
+
+@router.delete("/{flight_id}", response_model=bool)
+async def delete_flight(
+    flight_id: int,
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: FlightsQueries = Depends()
+) -> bool:
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not logged in"
+        )
+    return queries.delete(flight_id, user.id)
