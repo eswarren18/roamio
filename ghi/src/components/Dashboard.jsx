0bb5a7ff367cdd3f160f6a18d97f785bb568776d
Gregory <gregreinis@yahoo.com.here>
2025-01-26 11:37:36 -0800

    Google Maps Integration Without Key

--- ghi/src/components/Dashboard.jsx
+++ ghi/src/components/Dashboard.jsx
@@ -2,8 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthProvider';
 import { ModalContext } from './ModalProvider';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import React from 'react';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 
 function Dashboard() {
     const { toggleModal } = useContext(ModalContext);
@@ -16,11 +15,12 @@ function Dashboard() {
 
     const navToHome = () => {if (!isLoggedIn) {navigate("/")}}
 
-    const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
-    // console.log(apiKey);
+    const apiKey = "SecretKey" //import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    //console.log(apiKey);
+    //console.log(import.meta.env.MODE)
     //console.log('All env variables:', import.meta.env);
     // const GoogleMap = () => {
-    //     const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+    // const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
     const fetchTrips = async () => {
         try {
@@ -65,111 +65,100 @@ function Dashboard() {
     }, [trips]);
 
     return (
-        <div className="flex flex-col items-center m-8">
-            <div
-                id="map"
-                className="w-full"
-            >
-                <APIProvider apiKey={apiKey}>
-                    <Map
-                        style={{ width: '100%', height: '400px' }}
-                        defaultCenter={{ lat: 40.7128, lng: -74.0060 }}
-                        defaultZoom={10}
-                    />
-                    </APIProvider>
-
-                    {/* <APIProvider apiKey={apiKey}>
+            <div className="flex flex-col items-center m-8">
+                <div
+                    id="map"
+                    className="w-full"
+                >
+                    <APIProvider apiKey={apiKey}>
                         <Map
-                            style={{ width: "100%", height: "100%", borderRadius: "0.75rem" }}
-                            defaultCenter={{ lat: 40.6993, lng: -99.0817 }}
-                            defaultZoom={3}
-                            gestureHandling={"greedy"}
-                            disableDefaultUI={true}
+                            style={{ width: '100%', height: '400px' }}
+                            defaultCenter={{ lat: 40.7128, lng: -74.0060 }}
+                            defaultZoom={10}
                         />
-                    </APIProvider> */}
-            </div>
-
-            <div
-                id="filter"
-                className="flex flex-wrap content-start mt-6 mb-2 w-1/8 text-xl font-medium text-cyan-100 border-b border-cyan-100"
-            >
-                <button
-                    className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "all"
-                        ? "text-cyan-900 bg-cyan-100 active"
-                        : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
-                    }`}
-                    onClick={() => handleTripSelection("all")}
+                        </APIProvider>
+                </div>
+                <div
+                    id="filter"
+                    className="flex flex-wrap content-start mt-6 mb-2 w-1/8 text-xl font-medium text-cyan-100 border-b border-cyan-100"
                 >
-                    All
-                </button>
-                <button
-                    className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "upcoming"
-                        ? "text-cyan-900 bg-cyan-100 active"
-                        : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
-                    }`}
-                    onClick={() => handleTripSelection("upcoming")}
-                >
-                    Upcoming
-                </button>
-                <button
-                    className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "past"
-                        ? "text-cyan-900 bg-cyan-100 active"
-                        : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
-                    }`}
-                    onClick={() => handleTripSelection("past")}
-                >
-                    Past
-                </button>
-            </div>
-
-            <div
-                id="trip-cards"
-                className="grid grid-cols-4 w-full"
-            >
-                {selectedTrips.map((trip) => (
                     <button
-                        key={trip.id}
+                        className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "all"
+                            ? "text-cyan-900 bg-cyan-100 active"
+                            : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
+                        }`}
+                        onClick={() => handleTripSelection("all")}
+                    >
+                        All
+                    </button>
+                    <button
+                        className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "upcoming"
+                            ? "text-cyan-900 bg-cyan-100 active"
+                            : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
+                        }`}
+                        onClick={() => handleTripSelection("upcoming")}
+                    >
+                        Upcoming
+                    </button>
+                    <button
+                        className={`mx-2 inline-block px-4 py-2 rounded-t-lg ${activeButton === "past"
+                            ? "text-cyan-900 bg-cyan-100 active"
+                            : "bg-cyan-900 text-cyan-100 hover:bg-cyan-800"
+                        }`}
+                        onClick={() => handleTripSelection("past")}
+                    >
+                        Past
+                    </button>
+                </div>
+
+                <div
+                    id="trip-cards"
+                    className="grid grid-cols-4 w-full"
+                >
+                    {selectedTrips.map((trip) => (
+                        <button
+                            key={trip.id}
+                            className="flex flex-col text-cyan-900 bg-cyan-100 rounded-lg m-4 transform
+                            hover:bg-cyan-200 hover:scale-105 hover:ring-2 hover:ring-cyan-500
+                            transition-all duration-200 h-60"
+                            onClick={() => navigate(`/trip/${trip.id}`)}
+                        >
+                            <div className="w-full h-1/3">
+                                <img className="object-cover w-full h-full rounded-t-lg" src={trip.trip_image}></img>
+                            </div>
+                            <div className="flex flex-col items-center justify-center w-full h-2/3 px-2">
+                                <h1 className="font-bold text-2xl"> {trip.title}</h1>
+                                <p className="pt-2">
+                                    {trip.city}, {trip.country}
+                                </p>
+                            </div>
+                            <div className="flex flex-col justify-center w-full px-2">
+                                <div className="border-t border-black w-3/4 mx-auto justify-center"></div>
+                                <p className="my-2">
+                                    {trip.start_date} - {trip.end_date}
+                                </p>
+                            </div>
+                        </button>
+                    ))}
+                    <button
                         className="flex flex-col text-cyan-900 bg-cyan-100 rounded-lg m-4 transform
                         hover:bg-cyan-200 hover:scale-105 hover:ring-2 hover:ring-cyan-500
                         transition-all duration-200 h-60"
-                        onClick={() => navigate(`/trip/${trip.id}`)}
+                        onClick={() => toggleModal({ form: "AddTripModal" })}
                     >
                         <div className="w-full h-1/3">
-                            <img className="object-cover w-full h-full rounded-t-lg" src={trip.trip_image}></img>
+                            <img className="object-cover w-full h-full rounded-t-lg" src="/passport-stamps.png" alt="Passport Stamps Stock Image"></img>
                         </div>
-                        <div className="flex flex-col items-center justify-center w-full h-2/3 px-2">
-                            <h1 className="font-bold text-2xl"> {trip.title}</h1>
-                            <p className="pt-2">
-                                {trip.city}, {trip.country}
-                            </p>
+                        <div className="flex flex-col items-center justify-center w-full h-2/3">
+                            <h1 className="font-bold text-2xl mb-7">Add a Trip</h1>
                         </div>
-                        <div className="flex flex-col justify-center w-full px-2">
-                            <div className="border-t border-black w-3/4 mx-auto justify-center"></div>
-                            <p className="my-2">
-                                {trip.start_date} - {trip.end_date}
-                            </p>
+                        <div className="flex flex-col justify-center w-full">
+                            <div className="border-t border-black w-3/4 mx-auto justify-center mb-10"></div>
                         </div>
                     </button>
-                ))}
-                <button
-                    className="flex flex-col text-cyan-900 bg-cyan-100 rounded-lg m-4 transform
-                    hover:bg-cyan-200 hover:scale-105 hover:ring-2 hover:ring-cyan-500
-                    transition-all duration-200 h-60"
-                    onClick={() => toggleModal({ form: "AddTripModal" })}
-                >
-                    <div className="w-full h-1/3">
-                        <img className="object-cover w-full h-full rounded-t-lg" src="/passport-stamps.png" alt="Passport Stamps Stock Image"></img>
-                    </div>
-                    <div className="flex flex-col items-center justify-center w-full h-2/3">
-                        <h1 className="font-bold text-2xl mb-7">Add a Trip</h1>
-                    </div>
-                    <div className="flex flex-col justify-center w-full">
-                        <div className="border-t border-black w-3/4 mx-auto justify-center mb-10"></div>
-                    </div>
-                </button>
+                </div>
             </div>
-        </div>
 
-    );
-}
-export default Dashboard;
+        );
+    }
+    export default Dashboard;
