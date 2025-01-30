427f1f13f40d6ad9a37a540e4b752dede805caf0
David <davidiukuridze@gmail.com>
2025-01-30 15:52:59 -0800

    Added a non empty trips get request test.

--- api/tests/test_trips.py
+++ api/tests/test_trips.py
@@ -25,10 +25,25 @@ class CreateTripsQueries:
         result.update(trip)
         return result
 
+class NonEmptyTripsQueries:
+    def get_all(self, user_id):
+        return [
+            {
+                "id": 1,
+                "title": "Test Trip 1",
+                "country": "Country 1",
+                "city": "City 1",
+                "start_date": "2025-01-30",
+                "end_date": "2025-02-05",
+                "trip_image": "image1.png",
+                "user_id": user_id
+            }
+        ]
+
 def fake_get_jwt_user_data():
     return UserResponse(id=1, username="testuser")
 
-def test_get_trips():
+def test_get_empty_trips():
     # Arrange
     app.dependency_overrides[TripsQueries] = EmptyTripsQueries
     app.dependency_overrides[try_get_jwt_user_data] = fake_get_jwt_user_data
@@ -47,7 +62,7 @@ def test_create_trips():
     json = {
         "title": "Test Trip Creation",
         "country": "Test Country",
-        "city": "string",
+        "city": "Test City",
         "start_date": "2025-01-30",
         "end_date": "2025-01-30",
         "trip_image": "string"
@@ -56,7 +71,7 @@ def test_create_trips():
             "id": 1,
             "title": "Test Trip Creation",
             "country": "Test Country",
-            "city": "string",
+            "city": "Test City",
             "start_date": "2025-01-30",
             "end_date": "2025-01-30",
             "trip_image": "string",
@@ -64,6 +79,30 @@ def test_create_trips():
     }
     # Act
     response = client.post("/api/trips", json=json)
+    # Clean-up
+    app.dependency_overrides = {}
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == expected
+
+def test_get_non_empty_trips():
+    # Arrange
+    app.dependency_overrides[TripsQueries] = NonEmptyTripsQueries
+    app.dependency_overrides[try_get_jwt_user_data] = fake_get_jwt_user_data
+    expected = [
+        {
+            "id": 1,
+            "title": "Test Trip 1",
+            "country": "Country 1",
+            "city": "City 1",
+            "start_date": "2025-01-30",
+            "end_date": "2025-02-05",
+            "trip_image": "image1.png",
+            "user_id": 1
+        }
+    ]
+    # Act
+    response = client.get("/api/trips")
     # Clean-up
     app.dependency_overrides = {}
     # Assert
